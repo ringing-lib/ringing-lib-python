2e001e30a881ef68b221c722e590c057c56ea595
Leigh Simpson <code@simpleigh.com>
2014-12-26 14:16:01 +0000

    Added conversion so we can support Python 2.7 too

--- ringing-lib/row.pyx
+++ ringing-lib/row.pyx
@@ -3,12 +3,19 @@ from libcpp.string cimport string
 cimport row
 
 
+cdef bytes _s(s):
+    if type(s) is unicode:
+        return s.encode()
+    else:
+        return s
+
+
 cdef class Row:
 
     cdef row *thisptr
 
-    def __cinit__(self, unicode input):
-        self.thisptr = new row(input.encode())
+    def __cinit__(self, str input):
+        self.thisptr = new row(_s(input))
 
     def __dealloc__(self):
         del self.thisptr
