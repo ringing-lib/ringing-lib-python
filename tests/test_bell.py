5fd4e3a3be085834d1261e02bac93386f73d667e
Leigh Simpson <code@simpleigh.com>
2015-05-18 20:42:39 +0100

    More Python 2.6 compatibility fixes

--- tests/test_bell.py
+++ tests/test_bell.py
@@ -18,7 +18,7 @@ class BellTest(unittest.TestCase):
     def test_bell_constructor_exceptions(self):
         self.assertRaises(TypeError, lambda: Bell(self))
 
-        self.assertRaises(ValueError(-1))
+        self.assertRaises(ValueError, lambda: Bell(-1))
         Bell(0)
         Bell(MAX_BELL_NUMBER)
         self.assertRaises(ValueError, lambda: Bell(MAX_BELL_NUMBER + 1))
@@ -28,13 +28,13 @@ class BellTest(unittest.TestCase):
         self.assertRaises(ValueError, lambda: Bell(b'%'))
 
     def test_bell_bytes_bytes(self):
-        self.assertIs(type(bytes(Bell(5))), bytes)
+        self.assertEqual(type(bytes(Bell(5))), bytes)
 
     def test_bell_unicode_unicode(self):
         if sys.version_info[0] < 3:
-            self.assertIs(type(unicode(Bell(5))), unicode)
+            self.assertEqual(type(unicode(Bell(5))), unicode)
         else:
-            self.assertIs(type(Bell(5).__unicode__()), str)
+            self.assertEqual(type(Bell(5).__unicode__()), str)
 
     def test_is_symbol(self):
         self.assertTrue(Bell.is_symbol('1'))
